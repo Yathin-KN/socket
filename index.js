e3d1ff8a77ad20c55abc27c1dc6019193cfe1a8a
Yathin <yathin2184@gmail.com>
2023-03-15 15:14:37 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express=require('express')
+const http=require('http')
+const {Server}=require("socket.io")
+const cors=require('cors')
+require('dotenv').config()
+
+const app=express()
+
+app.use(cors())
+
+const port=process.env.PORT || 3001;
+
+const server=http.createServer(app)
+
+const io=new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"],
+    }
+})
+
+io.on("connection",(socket)=>{
+   console.log(`User connected: ${socket.id}`)
+
+   socket.on("send_message",(data)=>{
+        console.log(data)
+        socket.broadcast.emit("receive_message",data);
+   })
+})
+
+
+server.listen(port,()=>{
+    console.log(`Server running on http://localhost:${port}`)
+})
